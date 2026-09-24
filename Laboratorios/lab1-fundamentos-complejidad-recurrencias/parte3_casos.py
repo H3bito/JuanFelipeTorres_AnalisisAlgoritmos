18ca49518fc9d08abd3190b3467569491a0f4f3c
H3bito <juanphenex14@gmail.com>
2026-09-23 20:26:57 -0500

    Implementa insertion sort instrumentado

---
+++ Laboratorios/lab1-fundamentos-complejidad-recurrencias/parte3_casos.py
@@ -0,0 +1,10 @@
+from algoritmos import insertion_sort
+
+
+datos = [10, 20, 30, 40, 50]
+
+resultado, comparaciones = insertion_sort(datos)
+
+print("Lista original:", datos)
+print("Lista ordenada:", resultado)
+print("Comparaciones:", comparaciones)
